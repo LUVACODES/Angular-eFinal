a69ae569ba3c70e67b09693dfb40b1647866f91c
LUVACODES <l.vazquezrun@gmail.com>
2025-07-18 23:06:58 -0300

    add directive Bigtitle for font size

---
+++ src/shared/directives/big-title.spec.ts
@@ -0,0 +1,8 @@
+import { BigTitle } from "./big-title";
+
+describe('BigTitle', () => {
+  it('should create an instance', () => {
+    const directive = new BigTitle();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/shared/directives/big-title.ts
@@ -0,0 +1,18 @@
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appBigTitle]'
+})
+export class BigTitle {
+
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2
+  ) { }
+
+  ngOnInit() {
+    this.renderer.setStyle(this.el.nativeElement, 'font-size', '20px');
+   
+  }
+
+}
