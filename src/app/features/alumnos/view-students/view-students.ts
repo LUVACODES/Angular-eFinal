55954161dd6bf485f2543c3b568313469ee92edf
LUVACODES <l.vazquezrun@gmail.com>
2025-08-08 18:21:33 -0300

    added student layout in view-student

--- src/app/features/alumnos/view-students/view-students.ts
+++ src/app/features/alumnos/view-students/view-students.ts
@@ -2,15 +2,23 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Student } from '../../../../shared/entities';
 import { JsonPipe } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { FullnamePipe } from '../../../../shared/pipes/fullname-pipe';
 
 @Component({
   selector: 'app-view-students',
-  imports: [JsonPipe],
+  imports: [MatTableModule, MatCardModule, FullnamePipe],
   templateUrl: './view-students.html',
   styleUrl: './view-students.scss'
 })
 export class ViewStudents {
-
+    displayedColumns: string[] = [
+    'fullname',
+    'age',
+    'dni',
+    'average',
+   ];
     student: Student | undefined;
 
     constructor(private router: Router) {
