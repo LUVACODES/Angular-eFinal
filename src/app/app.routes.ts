b32bbb6aa55eb3666577ddbf518316f08d3df623
LUVACODES <l.vazquezrun@gmail.com>
2025-08-20 23:47:18 -0300

    change in routes to manage home to all users, and guard implementations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,27 +2,35 @@ import { Routes } from '@angular/router';
 import { RoutePaths } from '../shared/routes';
 import { Alumnos } from './features/alumnos/alumnos';
 import { NotFoundComponent } from './features/not-found-component/not-found-component';
+import { AuthGuard } from '../shared/guards/auth-guard-guard';
+
 
 export const routes: Routes = [
     {
         path: "", 
-        component: Alumnos
-    },
-    {
-        path: RoutePaths.ALUMNOS,
-        component: Alumnos,
+        redirectTo: RoutePaths.CURSOS,
+        pathMatch: "full"
     },
     {
         path: RoutePaths.CURSOS,
         loadComponent: () => import('./features/cursos/cursos').then(m => m.Cursos)
     },
+
+
+    {
+        path: RoutePaths.ALUMNOS,
+        component: Alumnos,
+        canActivate: [AuthGuard],
+    },
+ 
     {
         path: RoutePaths.INSCRIPCIONES,
         loadComponent: () => import('./features/inscripciones/inscripciones').then(m => m.Inscripciones)
     },
     {
         path: RoutePaths.VIEW_STUDENTS,
-        loadComponent: () => import('./features/alumnos/view-students/view-students').then (m => m.ViewStudents)
+        loadComponent: () => import('./features/alumnos/view-students/view-students').then (m => m.ViewStudents),
+        canActivate: [AuthGuard],
     },
     {
         path: RoutePaths.REGISTRATION,
--- src/shared/guards/auth-guard-guard.ts
+++ src/shared/guards/auth-guard-guard.ts
@@ -1,18 +1,17 @@
-import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { map, take } from 'rxjs/operators';
-import { selectUserRole } from '../../app/ngrx/usuario/usuario.selectors';
+import { selectUser } from '../../app/ngrx/usuario/usuario.selectors';
+import { Observable, map, take } from 'rxjs';
 
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  constructor(private store: Store) {}
 
-export const authGuard: CanActivateFn = (route, state) => {
-  const store = inject(Store);
-
-  return store.select(selectUserRole).pipe(
-    take(1),
-    map(role => {
-      if (role === 'admin') return true; 
-      return false; 
-    })
-  );
-};
+  canActivate(): Observable<boolean> {
+    return this.store.select(selectUser).pipe(
+      take(1),
+      map(user => !!user && user.role === 'admin')
+    );
+  }
+}
