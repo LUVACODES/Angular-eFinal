f04bfea5e65788fd61c9270f5d644c9abc3c7954
LUVACODES <l.vazquezrun@gmail.com>
2025-08-20 23:53:13 -0300

    added user logged information and logout button

--- src/app/toolbar/toolbar.ts
+++ src/app/toolbar/toolbar.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BigTitle } from '../../shared/directives/big-title';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { User } from '../ngrx/usuario/entities';
+import { selectUser } from '../ngrx/usuario/usuario.selectors';
+import * as UserActions from '../ngrx/usuario/usuario.actions';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [BigTitle],
+  imports: [BigTitle, CommonModule],
   templateUrl: './toolbar.html',
   styleUrl: './toolbar.scss'
 })
 export class Toolbar {
+  user$: Observable<User | null>;
 
+  constructor(private store: Store) {
+    this.user$ = this.store.select(selectUser);
+  }
+
+  logout() {
+    this.store.dispatch(UserActions.logout());
+  }
 }
